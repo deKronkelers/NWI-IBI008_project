fc7ed1b19353490700121eded57f3874aceb2d3e
Hendrikto <hendrik.to@gmail.com>
2017-01-15 14:12:26 +0100

    Indicate authorship.

--- filterHashtag.sh
+++ filterHashtag.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# author: Hendrik Werner s4549775
+
 grep -P "#\w+" < data/SentimentAnalysisDataset.csv > data/filteredHashtag.csv
--- prepareData.sh
+++ prepareData.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# author: Hendrik Werner s4549775
+
 # Extract only relevant information. This includes the sentiment and the
 # hashtags used for each tweet.
 cut -d, -f1,3 --complement data/filteredHashtag.csv\
