8461497f1203d051f9129f4779b78b9962537243
Hendrikto <hendrik.to@gmail.com>
2017-01-15 14:54:54 +0100

    Increase pattern specificity.

--- prepareData.sh
+++ prepareData.sh
@@ -4,7 +4,7 @@
 # Extract only relevant information. This includes the sentiment and the
 # hashtags used for each tweet.
 cut -d, -f1,3 --complement data/filteredHashtag.csv\
-	| grep -Po "^\d,|#\w+"\
+	| grep -Po "^[01],|#\w+"\
 	| tr "\n" " "\
 	| sed -r "s/ ([01],)/\n\1/g"\
 	> data/prepared.csv
