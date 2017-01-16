5867605ce39c06a2528d5fb8714ebed5ee1193c0
Hendrikto <hendrik.to@gmail.com>
2017-01-16 19:11:33 +0100

    Gather stats about hashtag count frequencies.

--- code/stats.sh
+++ code/stats.sh
@@ -27,6 +27,13 @@ awk '{print length}' <<< "$sortedHashtags" | sort -g | uniq -c\
 uniq -i <<< "$sortedHashtags" | awk '{print length}' | sort -g | uniq -c\
 	> ../data/stats/hashtagFrequencyLengthNoDup.txt
 
+hashtagCounts=""
+for nr in {2..20}; do
+	count=$(grep -iP "(#\w+\b.*?){$nr}" ../data/prepared.csv -c)
+	hashtagCounts="$hashtagCounts$nr $count"$'\n'
+done
+echo -n "$hashtagCounts" > ../data/stats/hashtagFrequenciesCount.txt 
+
 hashtagRepetitions=""
 for nr in {1..20}; do
 	count=$(grep -iP "(#\w+\b)(.*?\1\b){$nr}" ../data/prepared.csv -c)
