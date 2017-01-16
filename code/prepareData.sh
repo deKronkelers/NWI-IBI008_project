cf0291273dc2601b98a6dcd85c8261de531a0994
Hendrikto <hendrik.to@gmail.com>
2017-01-17 00:34:55 +0100

    Replace trailing space at eof with newline.

--- code/prepareData.sh
+++ code/prepareData.sh
@@ -5,6 +5,6 @@
 # hashtags used for each tweet.
 cut -d, -f1,3 --complement ../data/filteredHashtag.csv\
 	| grep -Po "^[01],|#\w+"\
-	| tr "\n" " "\
+	| paste -s -d" "\
 	| sed -r "s/ ([01],)/\n\1/g"\
 	> ../data/prepared.csv
