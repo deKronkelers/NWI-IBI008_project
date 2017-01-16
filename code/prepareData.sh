e2be74ba9a8fd5337475662055434e2cb5b3151a
Hendrikto <hendrik.to@gmail.com>
2017-01-17 00:43:13 +0100

    Remove unnecessary flag.

--- code/prepareData.sh
+++ code/prepareData.sh
@@ -5,6 +5,6 @@
 # hashtags used for each tweet.
 cut -d, -f1,3 --complement ../data/filteredHashtag.csv\
 	| grep -Po "^[01],|#\w+"\
-	| paste -s -d" "\
+	| paste -d" "\
 	| sed -r "s/ ([01],)/\n\1/g"\
 	> ../data/prepared.csv
