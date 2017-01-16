6925a26830f59709700ebe010c06cde28ed977f7
Hendrikto <hendrik.to@gmail.com>
2017-01-16 01:43:41 +0100

    Plot the frequency of hashtag lengths without duplicates.

--- code/plotStats.py
+++ code/plotStats.py
@@ -2,12 +2,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Plot the frequency of hashtag lengths
-hashtag_freq_len = np.loadtxt("../data/stats/hashtagFrequencyLength.txt")
 
-plt.plot(hashtag_freq_len[:, 1], hashtag_freq_len[:, 0])
-plt.title("Frequency of hashtag lengths")
-plt.xlabel("Hashtag length")
-plt.ylabel("Frequency")
-plt.savefig("../plots/hashtagFrequencyLength.pdf")
-plt.show()
+# Plot the frequency of hashtag lengths
+def plot_hashtag_freq_len(filename: str, title: str):
+    hashtag_freq_len = np.loadtxt("../data/stats/{}.txt".format(filename))
+    plt.plot(hashtag_freq_len[:, 1], hashtag_freq_len[:, 0])
+    plt.title(title)
+    plt.xlabel("Hashtag length")
+    plt.ylabel("Frequency")
+    plt.savefig("../plots/{}.pdf".format(filename))
+    plt.show()
+
+
+plot_hashtag_freq_len(
+    "hashtagFrequencyLength"
+    , title="Frequency of hashtag lengths"
+)
+plot_hashtag_freq_len(
+    "hashtagFrequencyLengthNoDup"
+    , title="Frequency of hashtag lengths (no duplicates)"
+)
