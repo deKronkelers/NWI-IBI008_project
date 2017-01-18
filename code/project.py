2fd800c309c3b41013ec02ab13037e5351bd088e
Hendrikto <hendrik.to@gmail.com>
2017-01-19 00:43:42 +0100

    Remove outliers from the data set.

--- code/project.py
+++ code/project.py
@@ -12,3 +12,18 @@ for line in open("../data/prepared.csv"):
         int(split_line[0])
         , set(split_line[1].split())
     ))
+
+# Remove outliers from the data set
+unique_hashtags = set()
+for line in open("../data/hashtagsUsedInSingleTweet.txt"):
+    unique_hashtags.add(line.strip())
+
+empty = set()
+for i, tweet in enumerate(data):
+    tweet.hashtags.difference_update(
+        {h for h in tweet.hashtags if h in unique_hashtags}
+    )
+    if not tweet.hashtags:
+        empty.add(i)
+
+data = [t for i, t in enumerate(data) if not i in empty]
