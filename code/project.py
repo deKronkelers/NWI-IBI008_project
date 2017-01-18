e6cc5985ce53548cc6902fb705791169364c4606
Hendrikto <hendrik.to@gmail.com>
2017-01-19 00:00:17 +0100

    Load the prepared data set.

--- code/project.py
+++ code/project.py
@@ -2,3 +2,13 @@
 from collections import namedtuple
 
 Tweet = namedtuple("DataPoint", ["sentiment", "hashtags"])
+
+data = []
+
+# Load the prepared data set
+for line in open("../data/prepared.csv"):
+    split_line = line.strip().split(sep=", ")
+    data.append(Tweet(
+        int(split_line[0])
+        , set(split_line[1].split())
+    ))
