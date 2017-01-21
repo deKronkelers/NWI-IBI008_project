eee635afa6a4b246ddc20c55e5f3c4a0c3e731c4
Hendrikto <hendrik.to@gmail.com>
2017-01-21 16:03:57 +0100

    Simplify code.

--- code/prepareData.sh
+++ code/prepareData.sh
@@ -3,8 +3,8 @@
 
 # Extract only relevant information. This includes the sentiment and the
 # hashtags used for each tweet.
-cut -d, -f1,3 --complement ../data/filteredHashtag.csv\
+cut -d, -f2,4- ../data/filteredHashtag.csv\
 	| grep -Po "^[01],|#\w+"\
-	| paste -d" "\
+	| paste -sd" "\
 	| sed -r "s/ ([01],)/\n\1/g"\
 	> ../data/prepared.csv
